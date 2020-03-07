e48c6a5b992656cee038799024054fd3a594ad92
Martin Štumpa <stumpa.martin@gmail.com>
2020-03-07 21:18:55 +0100

    feat(demo): update demo to the noew validation

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -10,10 +10,14 @@ export class AppComponent implements OnInit {
   title = 'Ngx-CZ-id';
 
   ctrl = new FormControl('', [Validators.required]);
+  ctrl2 = new FormControl('', [Validators.required]);
 
   ngOnInit() {
     this.ctrl.valueChanges.subscribe(val =>
       console.log('appCmp: ', val, this.ctrl.errors),
+    );
+    this.ctrl2.valueChanges.subscribe(val =>
+      console.log('appCmp2: ', val, this.ctrl2.errors),
     );
   }
 }
