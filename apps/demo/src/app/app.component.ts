31e79fb0c8ba21dabd453b2e812b3f47586d261b
Martin Štumpa <stumpa.martin@gmail.com>
2020-03-07 19:06:58 +0100

    feat(demo): add demo app

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ngx-cz-id-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'demo';
+export class AppComponent implements OnInit {
+  title = 'Ngx-CZ-id';
+
+  ctrl = new FormControl('', [Validators.required]);
+
+  ngOnInit() {
+    this.ctrl.valueChanges.subscribe(val =>
+      console.log('appCmp: ', val, this.ctrl.errors),
+    );
+  }
 }
--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxCzIdModule } from '@stumpam/ngx-cz-id';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, NgxCzIdModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
