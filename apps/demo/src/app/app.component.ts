24cd813871895b0e9790d54408e18e4f59689924
Martin Štumpa <stumpa.martin@gmail.com>
2020-03-09 21:10:41 +0100

    feat(demo): update demo app

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'ngx-cz-id-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit {
   title = 'Ngx-CZ-id';
 
-  ctrl = new FormControl('', [Validators.required]);
-  ctrl2 = new FormControl('', [Validators.required]);
+  ctrl = new FormControl('');
+  ctrl2 = new FormControl('');
+
+  form = new FormGroup({});
+  model = {};
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'id',
+      type: 'cz-id',
+      templateOptions: {
+        label: 'Id',
+        required: true,
+        attributes: {
+          inputmode: 'double',
+          autocomplete: 'off',
+        },
+        czIdOptions: {
+          emitInvalid: false
+        }
+      },
+    },
+  ];
 
   ngOnInit() {
     this.ctrl.valueChanges.subscribe(val =>
@@ -18,6 +45,9 @@ export class AppComponent implements OnInit {
     );
     this.ctrl2.valueChanges.subscribe(val =>
       console.log('appCmp2: ', val, this.ctrl2.errors),
+    );
+    this.form.valueChanges.subscribe(val =>
+      console.log('formly: ', val, this.form.errors),
     );
   }
 }
