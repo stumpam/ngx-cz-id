d35134d3ee6f796cf661d46e273e65a04ad5838a
Martin Štumpa <stumpa.martin@gmail.com>
2022-02-18 12:41:45 +0100

    fix: update lib config

--- libs/ngx-cz-id/src/index.ts
+++ libs/ngx-cz-id/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/ngx-cz-id.module';
 export * from './lib/interfaces/cz-id.interface';
+export * from './lib/directives/input/input.directive';
 export { isValidCzId, dateFromCzId } from './lib/functions/check.functions';
